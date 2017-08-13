4f8f39e257fbbaa00e75925e6ac232a8154df3c3
Matheus <silva.campos.matheus@gmail.com>
2017-08-12 21:15:38 -0300

    Código padronizado

--- EsquemaRelacional5.sql
+++ EsquemaRelacional5.sql
@@ -7,8 +7,8 @@ USE EMPRESA;
 
 -- criando tabelas
 CREATE TABLE PESSOA (
-	CODIGO_PESSOA INTEGER NOT NULL,
-	NOME_PESSOA VARCHAR(20) NOT NULL,
+	COD_PESSOA INTEGER NOT NULL,
+	NOME VARCHAR(20) NOT NULL,
 	ENDERECO VARCHAR(20) NOT NULL,
 	TIPO VARCHAR(20) NOT NULL,
 	ESPECIALIDADE VARCHAR(20),
@@ -91,11 +91,11 @@ alter table PECA add constraint pk_PECA primary key (COD_PECA);
 alter table SERVICO add constraint pk_SERVICO primary key (COD_SERVICO);
 alter table EQUIPE add constraint pk_EQUIPE primary key (ID_EQUIPE);
 alter table PERTENCE add constraint pk_PERTENCE primary key (ID_PERTENCE, COD_PERTENCE);
-alter table PESSOA add constraint pk_PESSOA primary key (CODIGO_PESSOA);
+alter table PESSOA add constraint pk_PESSOA primary key (COD_PESSOA);
 alter table DEMANDA add constraint pk_DEMANDA primary key (COD_ITEM_PECA,COD_ITEM_SERVICO);
 
 -- Chaves estrangeiras
-alter table VEICULO add constraint fk_VEICULO_PESSOA foreign key (COD_PESSOA) references PESSOA(CODIGO_PESSOA);
+alter table VEICULO add constraint fk_VEICULO_PESSOA foreign key (COD_PESSOA) references PESSOA(COD_PESSOA);
 alter table ORDEM_SERVICO add constraint fk_ORDEM_SERVICO_VEICULO foreign key (CHASSI) references VEICULO(CHASSI);
 alter table ORDEM_SERVICO add constraint fk_ORDEM_SERVICO_EQUIPE foreign key (ID_EQUIPE) references EQUIPE(ID_EQUIPE);
 alter table ITEM add constraint fk_ITEM_ORDEM_SERVICO foreign key (NUM_OS) references ORDEM_SERVICO(NUMERO_OS);
@@ -104,4 +104,4 @@ alter table PECA add constraint fk_PECA_ITEM foreign key (COD_PECA) references I
 alter table SERVICO add constraint fk_SERVICO_ITEM foreign key (COD_SERVICO) references ITEM(COD_ITEM);
 alter table DEMANDA add constraint fk_DEMANDA_ITEM foreign key (COD_ITEM_PECA,COD_ITEM_SERVICO) references ITEM(COD_ITEM);
 alter table PERTENCE add constraint fk_PERTENCE_EQUIPE foreign key (ID_EQUIPE) references EQUIPE(ID_EQUIPE);
-alter table PERTENCE add constraint fk_PERTENCE_PESSOA foreign key (COD_PESSOA) references EQUIPE(CODIGO_PESSOA);
+alter table PERTENCE add constraint fk_PERTENCE_PESSOA foreign key (COD_PESSOA) references EQUIPE(COD_PESSOA);
