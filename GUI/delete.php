c1dfe503ca47cbd34e59faf6cd0674ae4b374ae8
Matheus <silva.campos.matheus@gmail.com>
2017-08-18 17:35:46 -0300

    DELETE CONCLUÍDO

---
+++ GUI/delete.php
@@ -0,0 +1,26 @@
+<?php
+  require_once 'conexao.php';
+
+  if ($_GET['tabela']=='pessoa') {
+    mysqli_query($con, "DELETE FROM PESSOA WHERE COD_PESSOA = '".$_GET['id']."'");
+  } else if ($_GET['tabela']=='veiculo') {
+    mysqli_query($con, "DELETE FROM VEICULO WHERE CHASSI = '".$_GET['id']."'");
+  } else if ($_GET['tabela']=='os') {
+    mysqli_query($con, "DELETE FROM ORDEM_SERVICO WHERE NUMERO_OS = '".$_GET['id']."'");
+  } else if ($_GET['tabela']=='item') {
+    mysqli_query($con, "DELETE FROM ITEM WHERE COD_ITEM = '".$_GET['id']."'");
+  } else if ($_GET['tabela']=='equipe') {
+    mysqli_query($con, "DELETE FROM EQUIPE WHERE ID_EQUIPE = '".$_GET['id']."'");
+  }
+?>
+<html>
+  <head>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    <meta charset="utf-8">
+    <title>Deleted!</title>
+  </head>
+  <body>
+    <h1 class="text-primary text-center" onclick="window.location = 'read.php';">Oficina Esquema5</h1>
+  </body>
+</html>
